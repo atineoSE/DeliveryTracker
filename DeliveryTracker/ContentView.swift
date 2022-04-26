1b6e33af893d9ec4f9219a1e8d357cee368d38fb
Adrian Tineo <adrian@adriantineo.com>
2022-04-26 16:06:36 +0200

    Initial Commit

---
+++ DeliveryTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeliveryTracker
+//
+//  Created by Adrian Tineo Cabello on 26/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeliveryTracker/DeliveryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryTrackerApp.swift
+//  DeliveryTracker
+//
+//  Created by Adrian Tineo Cabello on 26/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
